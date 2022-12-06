0896de9e92aa882a6f6d2c561f9bc9b8b29c3d75
liyunfu <1511491804@qq.com>
2022-12-06 12:31:59 +0800

    feat: 添加标准化统一返回格式

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Version } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/')
+  @Version('1')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/')
+  @Version('2')
+  addHello(ctx) {
+    return this.appService.addHello(ctx);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import {
   FastifyAdapter,
   NestFastifyApplication,
@@ -10,6 +12,16 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+
+  // 标准化返回格式
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  // 接口版本化管理
+  app.enableVersioning({
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
+    type: VersioningType.URI,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
